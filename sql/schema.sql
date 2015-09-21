ba5c2fef90cff3bd3516ada9ec1f86fc777c9db2
cocodrips <cocodrips@gmail.com>
2015-09-21 06:19:20 +0000

    [Score: 3080] Add index to tables.

--- sql/schema.sql
+++ sql/schema.sql
@@ -13,3 +13,9 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `ip` varchar(255) NOT NULL,
   `succeeded` tinyint NOT NULL
 ) DEFAULT CHARSET=utf8;
+
+CREATE INDEX user_idx_login ON users (login);
+CREATE INDEX login_log_idx_user_id_succeeded_id ON login_log (user_id, succeeded, id);
+CREATE INDEX login_log_idx_user_id_ip_id ON login_log (user_id, ip, id);
+CREATE INDEX login_log_idx_succeeded_ip_id ON login_log (succeeded, ip, id);
+CREATE INDEX login_log_idx_ip_user_id ON login_log (ip, user_id);
